6f257ee4e4f121dc2ff4bf35c32a70d13eefa9b8
Averchenkov <nicroniys@gmail.com>
2020-04-23 02:02:33 +0300

    add pr10

---
+++ pr10/Assets/SceneGhange.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneGhange : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Application.LoadLevel("Scene1");
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Application.LoadLevel("Scene2");
+        }
+    }
+}
